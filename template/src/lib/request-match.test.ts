0fd903ac40f3e9404dc2e7d38a3fcc7edfd973aa
Shawn Kelly <skellyb@gmail.com>
2019-03-14 17:07:32 -0400

    jest

---
+++ template/src/lib/request-match.test.ts
@@ -0,0 +1,37 @@
+import { match } from "./request-match";
+
+class MockRequest {
+  url: string;
+  method: string;
+
+  constructor(url: string, options: { method: string }) {
+    this.url = url;
+    this.method = options.method;
+  }
+}
+
+test("match on path and method", () => {
+  const request = new MockRequest("https://postlight.com/favicon.ico", {
+    method: "GET"
+  });
+  const route0 = match(request as Request, "*", "/favicon.ico");
+  expect(route0).toBeTruthy();
+  const route1 = match(request as Request, "GET", "/favicon.ico");
+  expect(route1).toBeTruthy();
+  const route2 = match(request as Request, "OPTIONS", "/favicon.ico");
+  expect(route2).toBeUndefined();
+  const route3 = match(request as Request, "GET", "/");
+  expect(route3).toBeUndefined();
+});
+
+test("parse url params", () => {
+  const request = new MockRequest("https://postlight.com/user/123xyz", {
+    method: "GET"
+  });
+  const route = match(request as Request, "get", "/user/:id");
+  if (route) {
+    expect(route.params.id).toBe("123xyz");
+  } else {
+    fail(new Error("No match found"));
+  }
+});
