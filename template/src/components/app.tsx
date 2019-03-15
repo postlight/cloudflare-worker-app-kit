33fa09e3966aed18a0b99aabe883c71de9533cff
Shawn Kelly <skellyb@gmail.com>
2019-03-15 11:36:35 -0400

    replace start page

--- template/src/components/app.tsx
+++ template/src/components/app.tsx
@@ -3,7 +3,20 @@ import "./app.css";
 
 export const App = (): VNode => (
   <div>
-    <h1>Worker App</h1>
-    <img src="/assets/images/cf-logo-v.svg" />
+    <section class="content">
+      <h1>Hello, ☁️ Worker App!</h1>
+    </section>
+    <footer class="footer">
+      <p>A micro project from your friends at</p>
+      <a href="https://postlight.com/labs">
+        <img
+          id="logo"
+          src="/assets/images/postlight-labs.gif"
+          alt="Postlight Labs"
+          width={204}
+          height={45}
+        />
+      </a>
+    </footer>
   </div>
 );
